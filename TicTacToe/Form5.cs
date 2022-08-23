1177fb5aa8c8611784c8b9c331d020ad7ad4d3a8
errommore995@gmail.com <errommore995@gmail.com>
2022-08-23 12:41:29 +0600

    методы для игры 5 ан 5 из файла AI5x5

--- TicTacToe/Form5.cs
+++ TicTacToe/Form5.cs
@@ -26,6 +26,7 @@ namespace TicTacToe
                 }
             }
             setButton();
+            
         }
 
         private void setButton()
@@ -50,6 +51,7 @@ namespace TicTacToe
         private void button1_Click(object sender, EventArgs e)
         {
             sender.GetType().GetProperty("Text").SetValue(sender, "x");
+           // calculateBestMove(buttons);
         }
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -64,9 +66,358 @@ namespace TicTacToe
             }
         }
 
+        static void calculateBestMove(Button[,] buttons)
+        {
+            int bestValue = -100;
+            int pointI = 0;
+            int pointJ = 0;
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (buttons[i, j].Text == "O")
+                    {
+                        buttons[i, j].Text = "";
+                        int moveValue = minimax(buttons, false, 1000, -1000);
+                        buttons[i, j].Text = "";
+
+
+
+                        if (moveValue > bestValue)
+                        {
+                            pointI = i;
+                            pointJ = j;
+                            bestValue = moveValue;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static int evaluate(Button[,] buttons)
+        {
+            for (int i = 0; i < 5; i++) //Проверка столбцов на победу x, o
+            {
+                if (buttons[0, i] == buttons[1, i] &&
+                    buttons[1, i] == buttons[2, i] &&
+                    buttons[2, i] == buttons[3, i] &&
+                    buttons[3, i] == buttons[4, i]
+                    )
+                {
+                    if (buttons[0, i].Text == "o") { return 10; }
+                    if (buttons[0, i].Text == "x") { return -10; }
+                }
+            }
+
+
+            for (int i = 0; i < 5; i++) //Проверка строк на победу x, o
+            {
+                if (buttons[i, 0] == buttons[i, 1] &&
+                    buttons[i, 1] == buttons[i, 2] &&
+                    buttons[i, 2] == buttons[i, 3] &&
+                    buttons[i, 3] == buttons[i, 4]
+                    )
+                {
+                    if (buttons[i, 0].Text == "o") { return 10; }
+                    if (buttons[i, 0].Text == "x") { return -10; }
+                }
+            }
+
+            if (                                    //проверка диагонали слева направо
+                buttons[0, 0] == buttons[1, 1] &&
+                buttons[1, 1] == buttons[2, 2] &&
+                buttons[2, 2] == buttons[3, 3] &&
+                buttons[3, 3] == buttons[4, 4]
+                )
+            {
+                if (buttons[0, 0].Text == "o") { return 10; }
+                if (buttons[0, 0].Text == "x") { return -10; }
+            }
+            if (                                    //проверка диагонали справа налево
+                buttons[0, 4] == buttons[1, 3] &&
+                buttons[1, 3] == buttons[2, 2] &&
+                buttons[2, 2] == buttons[3, 1] &&
+                buttons[3, 1] == buttons[4, 0]
+                )
+            {
+                if (buttons[0, 4].Text == "o") { return 10; }
+                if (buttons[0, 4].Text == "x") { return -10; }
+            }
+            return 0;
+        }
+
+        static bool isEmptyCellsLeft(Button[,] buttons)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (buttons[i, j].Text == "")
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+
+        static int minimax(Button[,] buttons, Boolean isMax, int alpha, int beta)
+        {
+            int score = evaluate(buttons);
+
+            if (score == 10) return score;
+            if (score == -10) return score;
+            if (isEmptyCellsLeft(buttons) == false)
+                return 0;
+
+
+            int value = 0;
+
+            if (isMax)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (buttons[i, j].Text == "")
+                        {
+                            buttons[i, j].Text = "o";
+                            value = minimax(buttons, !isMax, alpha, beta);
+                            buttons[i, j].Text = "o";
+
+                            if (beta <= alpha)
+                            {
+                                break;
+                            }
+
+                            if (value >= beta)
+                            {
+                                return value;
+                            }
+                        }
+                    }
+                }
+                return value;
+            }
+
+
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (buttons[i, j].Text == "")
+                        {
+                            buttons[i, j].Text = "x";
+                            value = minimax(buttons, isMax, alpha, beta);
+                            buttons[i, j].Text = "";
+
+                            if (value <= alpha)
+                            {
+                                return value;
+                            }
+
+                            if (value < beta)
+                            {
+                                beta = value;
+                            }
+                        }
+                    }
+                }
+                return value;
+            }
+
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+    }
+
+    /// <summary>
+    /// //////////////////////////////////////////////////////////////
+    /// </summary>
+    public class AI5x5
+    {
+        public int pointI;
+        public int pointJ;
+
+        public static AI5x5 calculateBestMove(char[,] buttons)
+        {
+            int bestValue = -100;
+            AI5x5 aiMove = new AI5x5();
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (buttons[i, j] == 'z')
+                    {
+                        buttons[i, j] = 'o';
+                        int moveValue = minimax(buttons, false, 1000, -1000);
+                        buttons[i, j] = 'z';
+
+
+
+                        if (moveValue > bestValue)
+                        {
+                            aiMove.pointI = i;
+                            aiMove.pointJ = j;
+                            bestValue = moveValue;
+                        }
+                    }
+                }
+
+            }
+            return aiMove;
+        }
+
+        public static int evaluate(char[,] buttons)
+        {
+            for (int i = 0; i < 5; i++) //Проверка столбцов на победу x, o
+            {
+                if (buttons[0, i] == buttons[1, i] &&
+                    buttons[1, i] == buttons[2, i] &&
+                    buttons[2, i] == buttons[3, i] &&
+                    buttons[3, i] == buttons[4, i]
+                    )
+                {
+                    if (buttons[0, i] == 'o') { return 10; }
+                    if (buttons[0, i] == 'x') { return -10; }
+                }
+            }
+
+
+            for (int i = 0; i < 5; i++) //Проверка строк на победу x, o
+            {
+                if (buttons[i, 0] == buttons[i, 1] &&
+                    buttons[i, 1] == buttons[i, 2] &&
+                    buttons[i, 2] == buttons[i, 3] &&
+                    buttons[i, 3] == buttons[i, 4]
+                    )
+                {
+                    if (buttons[i, 0] == 'o') { return 10; }
+                    if (buttons[i, 0] == 'x') { return -10; }
+                }
+            }
+
+            if (                                    //проверка диагонали слева направо
+                buttons[0, 0] == buttons[1, 1] &&
+                buttons[1, 1] == buttons[2, 2] &&
+                buttons[2, 2] == buttons[3, 3] &&
+                buttons[3, 3] == buttons[4, 4]
+                )
+            {
+                if (buttons[0, 0] == 'o') { return 10; }
+                if (buttons[0, 0] == 'x') { return -10; }
+            }
+            if (                                    //проверка диагонали справа налево
+                buttons[0, 4] == buttons[1, 3] &&
+                buttons[1, 3] == buttons[2, 2] &&
+                buttons[2, 2] == buttons[3, 1] &&
+                buttons[3, 1] == buttons[4, 0]
+                )
+            {
+                if (buttons[0, 4] == 'o') { return 10; }
+                if (buttons[0, 4] == 'x') { return -10; }
+            }
+            return 0;
+        }
+
+        static bool isEmptyCellsLeft(char[,] buttons)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (buttons[i, j] == 'z')
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+
+        static int minimax(char[,] buttons, Boolean isMax, int alpha, int beta)
+        {
+            int score = evaluate(buttons);
+
+            if (score == 10) return score;
+            if (score == -10) return score;
+            if (isEmptyCellsLeft(buttons) == false)
+                return 0;
+
+
+            int value = 0;
+            if (isMax)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (buttons[i, j] == 'z')
+                        {
+                            buttons[i, j] = 'o';
+                            value = minimax(buttons, !isMax, alpha, beta);
+                            buttons[i, j] = 'z';
+
+                            if (beta <= alpha)
+                            {
+                                break;
+                            }
+
+
+                            if (value >= beta)
+                            {
+                                return value;
+                            }
+                        }
+                    }
+                }
+                return value;
+            }
+
+
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (buttons[i, j] == 'z')
+                        {
+                            buttons[i, j] = 'x';
+                            value = minimax(buttons, isMax, alpha, beta);
+                            buttons[i, j] = 'z';
+
+
+                            if (value <= alpha)
+                            {
+                                return value;
+                            }
+
+
+
+                            if (value < beta)
+                            {
+                                beta = value;
+                            }
+
+                        }
+                    }
+                }
+                return value;
+            }
+
+        }
+
+
     }
 }
